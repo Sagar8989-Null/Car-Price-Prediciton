044e98477b1fb40e65af6b03c596df8580309eb0
Sagar Jamadar <sagarjamadar8989@gmail.com>
2025-08-17 00:33:25 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -6,35 +6,19 @@ import numpy as np
 app = Flask(__name__)
 CORS(app) 
 
-# Load your trained model
+# Loading trained model
 model = joblib.load("Model/car_price_model.pkl")
-
-# def predict():
-#     data = request.json  # Get JSON data from frontend
-    
-#     # Extract values (make sure keys match frontend)
-#     year = int(data["year"])
-#     km_driven = int(data["km_driven"])
-#     fuel = int(data["fuel"])  
-#     transmission = int(data["transmission"])
-#     owner = int(data["owner"])
-
-#     # Example: Adjust this feature order based on how you trained your model
-#     features = np.array([[year, km_driven, fuel, transmission,owner]])
-
-#     prediction = model.predict(features)[0]
-    
-#     return jsonify({"predicted_price": round(float(prediction), 2)})
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json  # Get JSON data from frontend
     
-    # Extract values (make sure keys match frontend)
+    # Extract values 
     year = int(data["year"])
     km_driven = int(data["km_driven"])
 
+    # Adujusting lists according to Values
     if data["fuel"] == "Diesel":
         fuel=[1,0]
     elif data["fuel"] == "Petrol":
@@ -54,12 +38,11 @@ def predict():
     else:
         owner = [1,0,0]
 
+    # Passing all the Features in order as the model is trained
     features = np.array([[km_driven,fuel[0],fuel[1],transmission,owner[0],owner[1],owner[2],year]])
-
+    # actual Prediction
     prediction = model.predict(features)[0]
 
-    print(prediction)
-    
     return jsonify({"predicted_price": round(float(prediction), 2)})
 
 if __name__ == "__main__":
